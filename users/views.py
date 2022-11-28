b6484529ad72aa7aabc3314fef354d60d936711e
noel-ellis <noel_ellis@proton.me>
2022-11-28 20:03:54 +0100

    profile editing

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserSignUpForm
+from .forms import UserSignUpForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -19,4 +19,32 @@ def signup(request):
 
 @login_required
 def settings(request):
-    return render(request, "users/settings.html")
+    if request.method == "POST":
+        user_form = UserUpdateForm(
+            request.POST, 
+            instance=request.user
+        )
+        profile_form = ProfileUpdateForm(
+            request.POST, 
+            request.FILES, 
+            instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Updated')
+            return redirect('users:settings')
+        
+        messages.error(request, f'Data is invalid')
+        return redirect('users:settings')
+
+    user_form = UserUpdateForm(instance=request.user)
+    profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, "users/settings.html", context)
