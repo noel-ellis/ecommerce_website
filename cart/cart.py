3f65cfa67f747e93d9a5f1972f45d7def9950a9f
noel-ellis <noel_ellis@proton.me>
2023-01-27 15:41:32 +0100

    basic cart logic 0.1

--- cart/cart.py
+++ cart/cart.py
@@ -8,16 +8,21 @@ class Cart:
 
     def __init__(self, request):
         self.session = request.session
-        if 'skey' not in request.session:
+        if 'skey' not in self.session:
             self.cart = self.session['skey'] = {}
             return
         self.cart = self.session.get('skey')
 
-    def add(self, product: Product):
+    def add(self, product: Product, product_qty: int):
         product_id = product.id
         if product_id not in self.cart:
             self.cart[product_id] = {
+                'product_qty': product_qty,
                 'price': product.price
             }
 
+        # Updates DB w/ cart data
         self.session.modified = True
+
+    def __len__(self):
+        return sum(item['product_qty'] for item in self.cart.values())
--- cart/views.py
+++ cart/views.py
@@ -13,9 +13,12 @@ def add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
-        response = JsonResponse({'test': 'data'})
+        cart.add(product=product, product_qty=product_qty)
+
+        cart_qty = cart.__len__()
+        response = JsonResponse({'productqty': cart_qty})
         return response
 
     
