3308e90593010804770c3bc7de81e4d9d05652f3
noel-ellis <noel_ellis@proton.me>
2023-06-29 11:02:39 +0200

    trying static fix #1

--- ecommerce_website/urls.py
+++ ecommerce_website/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include('store.urls', namespace='store')),
@@ -16,5 +15,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings .MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
