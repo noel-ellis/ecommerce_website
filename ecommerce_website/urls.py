27fb5b3f8287fbc843f22c74a78094efe5b2a1b7
noel-ellis <noel_ellis@proton.me>
2023-06-29 12:16:48 +0200

    moved images 1

--- ecommerce_website/urls.py
+++ ecommerce_website/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include('store.urls', namespace='store')),
@@ -14,7 +13,4 @@ urlpatterns = [
     path("admin/", admin.site.urls)
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
