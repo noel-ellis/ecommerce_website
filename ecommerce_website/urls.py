838c01bcc99a010156d0bb23f50d12afae763b73
noel-ellis <noel_ellis@proton.me>
2023-06-29 12:19:07 +0200

    revert back to roughly ec70285

--- ecommerce_website/urls.py
+++ ecommerce_website/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include('store.urls', namespace='store')),
@@ -13,4 +14,4 @@ urlpatterns = [
     path("admin/", admin.site.urls)
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
